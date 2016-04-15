28a42047ad7eb46073663d1a46122592a9887252
jssalamanca1967 <js.salamanca1967@uniandes.edu.co>
2016-04-15 11:18:08 -0500

    Update crontab.sh

--- Grupo02-201610/cron/crontab.sh
+++ Grupo02-201610/cron/crontab.sh
@@ -1,7 +1,7 @@
 
 AWSBucket=designmatchc
-AWSAccessKeyId=AKIAJCN5WTFUUZNLH72Q
-AWSSecretKey=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
+AWSAccessKeyId=
+AWSSecretKey=
 PATH=/home/ubuntu/.rbenv/plugins/ruby-build/bin:/home/ubuntu/.rbenv/shims:/home/ubuntu/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 SHELL=/bin/bash
 
