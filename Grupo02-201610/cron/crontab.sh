05e3e76772d8af469040880e30f4d2518f3d0cd7
Ubuntu <ubuntu@ip-172-31-21-120.us-west-2.compute.internal>
2016-04-15 16:09:42 +0000

    yay

---
+++ Grupo02-201610/cron/crontab.sh
@@ -0,0 +1,8 @@
+
+AWSBucket=designmatchc
+AWSAccessKeyId=AKIAJCN5WTFUUZNLH72Q
+AWSSecretKey=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
+PATH=/home/ubuntu/.rbenv/plugins/ruby-build/bin:/home/ubuntu/.rbenv/shims:/home/ubuntu/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+SHELL=/bin/bash
+
+* * * * * cd /home/ubuntu/DespliegueBWeb && rails runner cron/script.rb
