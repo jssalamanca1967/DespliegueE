4f72e4b024dacf74ed9b7fea9cc849cb2fc356a2
jssalamanca1967 <js.salamanca1967@uniandes.edu.co>
2016-04-16 21:30:29 -0500

    memcache 1

--- Grupo02-201610/config/initializers/session_store.rb
+++ Grupo02-201610/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes
+#Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes
 
 # Be sure to restart your server when you modify this file.
 
-#Rails.application.config.session_store :cookie_store, key: '_DesarrolloCloud1_session'
+Rails.application.config.session_store :cookie_store, key: '_DesarrolloCloud1_session'
