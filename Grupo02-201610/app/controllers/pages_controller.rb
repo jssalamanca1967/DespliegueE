4fd3ae2de2ed8f993106002b16abdef020c882e7
jssalamanca1967 <js.salamanca1967@uniandes.edu.co>
2016-04-17 19:42:12 -0500

    Mail

--- Grupo02-201610/app/controllers/pages_controller.rb
+++ Grupo02-201610/app/controllers/pages_controller.rb
@@ -30,25 +30,7 @@ class PagesController < ApplicationController
   end
 
   def about
-    require 'mail'
 
-    Mail.defaults do
-      delivery_method :smtp, {
-        :address => 'smtp.sendgrid.net',
-        :port => '587',
-        :domain => 'heroku.com',
-        :user_name => ENV['SENDGRID_USERNAME'],
-        :password => ENV['SENDGRID_PASSWORD'],
-        :authentication => :plain,
-        :enable_starttls_auto => true
-      }
-    end
-
-    Mail.deliver do
-  to 'example@example.com'
-  from 'sender@example.comt'
-  subject 'testing send mail'
-  body 'Sending email with Ruby through SendGrid!'
-end
+    SenderMail.enviarHeroku().deliver_now
   end
 end
